ebb2055793c1d864a792100e92a0ce5919a209b2
Juan <j.juancamilojurado@gmail.com>
2019-09-08 13:00:40 -0500

    Creacion del programa

---
+++ ProgressBarYDatagridview/OpcionDatagridview.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace ProgressBarYDatagridview
+{
+    partial class OpcionDatagridview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OpcionDatagridview));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtcedula = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtedad = new System.Windows.Forms.TextBox();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtfila = new System.Windows.Forms.TextBox();
+            this.btnborrar = new System.Windows.Forms.Button();
+            this.dtregistros = new System.Windows.Forms.DataGridView();
+            this.Cedula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Edad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnsalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtregistros)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Uso de Datagridview";
+            // 
+            // txtcedula
+            // 
+            this.txtcedula.Location = new System.Drawing.Point(92, 81);
+            this.txtcedula.Name = "txtcedula";
+            this.txtcedula.Size = new System.Drawing.Size(110, 20);
+            this.txtcedula.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cédula:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nombre:";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(92, 129);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(175, 20);
+            this.txtnombre.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Edad:";
+            // 
+            // txtedad
+            // 
+            this.txtedad.Location = new System.Drawing.Point(92, 181);
+            this.txtedad.Name = "txtedad";
+            this.txtedad.Size = new System.Drawing.Size(44, 20);
+            this.txtedad.TabIndex = 5;
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnguardar.Location = new System.Drawing.Point(161, 179);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(82, 26);
+            this.btnguardar.TabIndex = 7;
+            this.btnguardar.Text = "Guardar.";
+            this.btnguardar.UseVisualStyleBackColor = true;
+            this.btnguardar.Click += new System.EventHandler(this.btnguardar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 11.25F);
+            this.label5.Location = new System.Drawing.Point(322, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Fila:";
+            // 
+            // txtfila
+            // 
+            this.txtfila.Location = new System.Drawing.Point(361, 79);
+            this.txtfila.Name = "txtfila";
+            this.txtfila.Size = new System.Drawing.Size(58, 20);
+            this.txtfila.TabIndex = 9;
+            // 
+            // btnborrar
+            // 
+            this.btnborrar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnborrar.Location = new System.Drawing.Point(436, 77);
+            this.btnborrar.Name = "btnborrar";
+            this.btnborrar.Size = new System.Drawing.Size(75, 22);
+            this.btnborrar.TabIndex = 10;
+            this.btnborrar.Text = "Borrar.";
+            this.btnborrar.UseVisualStyleBackColor = true;
+            this.btnborrar.Click += new System.EventHandler(this.btnborrar_Click);
+            // 
+            // dtregistros
+            // 
+            this.dtregistros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtregistros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Cedula,
+            this.Nombre,
+            this.Edad});
+            this.dtregistros.Location = new System.Drawing.Point(17, 241);
+            this.dtregistros.Name = "dtregistros";
+            this.dtregistros.Size = new System.Drawing.Size(391, 184);
+            this.dtregistros.TabIndex = 11;
+            this.dtregistros.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtregistros_CellClick);
+            // 
+            // Cedula
+            // 
+            this.Cedula.HeaderText = "Cédula";
+            this.Cedula.Name = "Cedula";
+            this.Cedula.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            this.Nombre.Width = 200;
+            // 
+            // Edad
+            // 
+            this.Edad.HeaderText = "Edad";
+            this.Edad.Name = "Edad";
+            this.Edad.ReadOnly = true;
+            this.Edad.Width = 45;
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(398, 179);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(100, 27);
+            this.btnsalir.TabIndex = 12;
+            this.btnsalir.Text = "Salir.";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OpcionDatagridview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 464);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.dtregistros);
+            this.Controls.Add(this.btnborrar);
+            this.Controls.Add(this.txtfila);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnguardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtedad);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtcedula);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "OpcionDatagridview";
+            this.Text = "OpcionDatagridview";
+            ((System.ComponentModel.ISupportInitialize)(this.dtregistros)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtcedula;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtedad;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtfila;
+        private System.Windows.Forms.Button btnborrar;
+        private System.Windows.Forms.DataGridView dtregistros;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cedula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Edad;
+        private System.Windows.Forms.Button btnsalir;
+    }
+}
